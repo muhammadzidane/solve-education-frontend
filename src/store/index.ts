9f81a3db62834cd787615fe30610e3650da0ec58
Zidane <muhzidanealsaadawi@gmail.com>
2024-07-06 20:26:43 +0700

    feat: question persist with localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import { createQuestionSlice } from "./question";
 import { QuestionSlice } from "./question/question-store";
@@ -6,6 +7,6 @@ import { createToastSlice } from "./toast";
 import { ToastSlice } from "./toast/toast-store";
 
 export const useBoundStore = create<ToastSlice & QuestionSlice>((...a) => ({
+  ...persist(createQuestionSlice, { name: "question-storage" })(...a), // 'localStorage'
   ...createToastSlice(...a),
-  ...createQuestionSlice(...a),
 }));
